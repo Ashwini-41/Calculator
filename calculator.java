4a601fb4dcf77f04f48742528d8369fe949c44db
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-08-03 10:46:29 +0530

    Addition : Addition Function is added

--- calculator.java
+++ calculator.java
@@ -2,5 +2,10 @@
 public class calculator{
     public static void main(String[] args) {
         System.out.println(" Welcome to calculator ");
+        int a = 10;
+        int b = 20;
+
+        int c = a + b;
+        System.out.println("Addition is : " + c);
     }
 }
